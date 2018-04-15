cf1985d905a42c39dc61e1ff5e760f38cd2140c9
Nicacioneto <nicacionetobsb@hotmail.com>
2018-04-15 14:36:11 -0300

    Fixing routes and adding controller methods

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -1,2 +1,32 @@
 class SensorsController < ApplicationController
+  before_action :set_sensor, only: [:show]
+
+  # GET /sensors
+  def index
+    @sensors = Sensor.all
+    json_response(@sensors)
+  end
+
+  # POST /sensors
+  def create
+    @sensor = Sensor.create!(sensor_params)
+    json_response(@sensor, :created)
+  end
+
+  # GET /sensors/:id
+  def show
+    json_response(@sensor)
+  end
+
+  private
+
+  def sensor_params
+    # whitelist params
+    params.permit(:name)
+  end
+
+  def set_sensor
+    @sensor = Sensor.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :sensor, except: [:edit, :update, :destroy] do
-    resources :data_collect, except: [:edit, :update, :destroy]
+  resources :sensors, except: [:edit, :update, :destroy] do
+    resources :data_collects, except: [:edit, :update, :destroy]
   end
 end
